99c031d1a1fb788bc337d12e39eec614105d694a
SergeiTsipliaev <142197512+SergeiTsipliaev@users.noreply.github.com>
2024-09-18 20:05:38 +0800

    add check for side triangle

--- src/triangle.py
+++ src/triangle.py
@@ -3,18 +3,17 @@ from abc import ABC
 
 
 class Triangle(Figure, ABC):
-    def __init__(self, side_a, side_b, side_c, height):
-        if side_a <= 0 or side_b <= 0 or side_c <= 0 or height <= 0:
+    def __init__(self, side_a, side_b, side_c):
+        if (side_a + side_b) < side_c or (side_b + side_c) < side_a or (side_c + side_a) < side_b:
             raise ValueError("It is impossible to create such a triangle!!!")
         self.side_a = side_a
         self.side_b = side_b
         self.side_c = side_c
-        self.height = height
 
 
     @property
     def get_area(self):
-        return 0.5 * self.side_a + self.height
+        return 0.5 * self.side_a + self.side_b
 
 
     @property
